ffd89299010aae1bf4d3166d1823287fc9ffd190
Joseph Fan <joefan@joe-fan.com>
2017-08-17 22:00:06 -0600

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+    var render = function(){
+        $('#todoList').empty()
+        for (var i = 0; i < todoList.length; i++ ) {
+            if (todoList[i].complete === false) {
+                $('#todoList').append(`<li id="${todoList[i]._id}" class="task"><button btn-task-number="${todoList[i]._id}" class="taskButton">Delete</button> ${todoList[i]['todoText']}`)
+            } else {
+                $('#todoList').append(`<li id="${todoList[i]._id}" class="task lineThrough"><button btn-task-number="${todoList[i]._id}" class="taskButton">Delete</button> ${todoList[i]['todoText']}</li>`)
+            }
+        }
+    }
+
+    var delayMillis = 500;
+    
+    var getFreshData = function(){
+        $.get('/todo', function(data){
+            console.log(data)
+            todoList = data
+            setTimeout(function(){ // timeout set to try to remedy the render issue functions work but rendering is slow
+                render()
+            }, delayMillis)
+        })
+    }
+
+    var todoList = []
+    getFreshData()
+
+    $('body').on('click', '.taskButton', function(event){
+        event.preventDefault()
+        var btnTaskNumber = $(event.target).attr('btn-task-number')
+        var btnItem = document.getElementById(btnTaskNumber)
+        console.log("Button Clicked", btnTaskNumber, btnItem)
+        $.post('/todo/delete', $(this).serialize() + '_id=' + btnTaskNumber, function(data){})
+        getFreshData()
+    })
+
+    $('body').on('click', '.task', function(event){
+        event.preventDefault()
+        var dataTaskNumber = $(event.target).attr('id')
+        var listItem = document.getElementById(dataTaskNumber)
+        if (listItem.className.includes('lineThrough')){
+            $.post('/todo/upd-false', $(this).serialize() + '_id=' + dataTaskNumber, function(data){})
+            getFreshData()
+        } else {
+            $.post('/todo/upd-true', $(this).serialize() + '_id=' + dataTaskNumber, function(data){})
+            getFreshData()
+        }
+    })
+
+    $('#newTodoForm').on('submit', function(event){
+        event.preventDefault()
+        console.log('#newTodoForm this',$( this ).serialize())
+        $.post('/todo', $(this).serialize(), function(data){
+            console.log('#newTodoForm data ',data)
+            getFreshData()
+        })
+    })
+})
---
+++ server.js
@@ -0,0 +1,94 @@
+var express     = require('express')
+var app         = express()
+var bodyParser  = require('body-parser')
+var mongoose    = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/todoApp')
+
+// ObjectID = mongoose.ObjectID
+
+var todoSchema = mongoose.Schema({
+    todoText: {
+        type    : String,
+        required: true
+    },
+    complete: {
+        type    : Boolean,
+        default : false
+    },
+})
+
+var TodoModel = mongoose.model('todo', todoSchema, 'todo')
+
+app.use(express.static('./public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.get('/', function(req, res){
+    res.sendFile('./html/index.html', {root: './public'})
+})
+
+app.get('/todo', function(req, res, next){
+    TodoModel.find({}, function(err, docs){
+        if ( err ) { next(err) }
+        else {
+            res.send(docs) 
+        }
+    })
+})
+
+app.post('/todo', function(req, res, next){   
+    var newTodo = new TodoModel(req.body)
+    newTodo.save(function(err){
+        if (err){ next(err) }
+        else {
+            res.send({success:'success!'})
+        }
+    })
+})
+
+app.post('/todo/upd-false', function(req, res, next){
+    TodoModel.find({_id: req.body}, function(err, docs){
+        if(err) {next(err)}
+        else {
+            docs[0]["complete"] = false
+            docs[0].save(function(err){
+                if ( err ) { next(err) }
+            })
+        }
+        res.send({success:'success!'})
+    })
+})
+
+app.post('/todo/upd-true', function(req, res, next){
+    TodoModel.find({_id: req.body}, function(err, docs){ 
+        if(err) {next(err)}
+        else {
+            docs[0]["complete"] = true
+            docs[0].save(function(err){
+                if ( err ) { next(err) }
+            })
+        }
+        res.send({success:'success!'})
+    })
+})
+
+app.post('/todo/delete', function(req, res, next){
+    console.log("DELETE req.body: ", req.body)
+    TodoModel.find({_id: req.body}, function(err, docs){
+        if(err) {next(err)}
+        else {
+            docs[0]["complete"] = true
+            docs[0].remove(function(err){
+                if ( err ) { next(err) }
+            })
+        }
+        res.send({success:'success!'})
+    })
+})
+// error handling
+app.use(function(err, req, res, next){
+    console.log('something went wrong: ', err)
+    res.send(err)
+})
+app.listen(8080)
